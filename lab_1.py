f480a25c7e6f3db1cce19c5686086900f0e6b5ce
steve-A7 <steve900032@gmail.com>
2026-02-09 10:01:44 +0600

    modified for the first time

--- lab_1.py
+++ lab_1.py
@@ -1,3 +1,18 @@
-import pandas as pd
+import copy
 
-print(pd.__version__)
+a=[[1,2],[3,4]]
+
+b=copy.deepcopy(a)
+b[0].append(5)
+
+print(id(a))
+print(id(b))
+
+print(a)
+print(b)
+
+c=10
+
+c + 2
+
+print(c)
