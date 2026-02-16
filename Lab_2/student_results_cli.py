3ce0d95956a0d6db5bb10dc9b394a639990a64a7
steve-A7 <steve900032@gmail.com>
2026-02-16 10:09:28 +0600

    Added a new student result script for CLI

---
+++ Lab_2/student_results_cli.py
@@ -0,0 +1,50 @@
+"""
+Docstring for student_results_cli
+This is an application for calculating and storing students results.
+This will run in CLI.
+If possible we will also save it in a .csv file.
+"""
+
+def print_menu() -> None:
+   print("---- Student Result Calculator ----")
+   print("1) Add student result + calculate results")
+   print("2) List all student results")
+   print("3) Search student by Id")
+   print("4) Delete student by Id")
+   print("5) Save results to .csv file")
+   print("6) Exit")
+
+def main() -> None:
+   while True:
+      print_menu()
+      choice: str = input("Enter your choice (1-6): ").strip()
+      #Match case is only available in Python 3.10 and later
+      match choice:
+         case "1":
+            print("Adding student result...")
+            #Here add and student result logic will be implemented
+            pass
+         case "2":
+            print("Listing all student results...")
+            #Here list all student results logic will be implemented
+            pass
+         case "3":
+            print("Searching student by Id...")
+            #Here search student by Id logic will be implemented
+            pass
+         case "4":
+            print("Deleting student by Id...")
+            #Here delete student by Id logic will be implemented
+            pass
+         case "5":
+            print("Saving results to .csv file...")
+            #Here save results to .csv file logic will be implemented
+            pass
+         case "6":
+            print("Exiting the application...")
+            break
+         case _:
+            print("Invalid choice. Please enter a number between 1 and 6.")
+
+if __name__ == "__main__":
+   main()
